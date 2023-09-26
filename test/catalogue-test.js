8416fe9cd5630f75fe7470a11c08831ce9bebd47
Dominik Kawka <dominikkawka2002@gmail.com>
2023-09-26 10:04:42 +0100

    Check reorders fully implemented and tested.

--- test/catalogue-test.js
+++ test/catalogue-test.js
@@ -9,7 +9,7 @@ let batch = null;
 describe("Catalogue", () => {
   beforeEach( () => {
     cat = new Catalogue("Test Catalogue");
-    
+
     cat.addProduct(new Product("A123", "Product 1", 100, 10, 10.0));
     cat.addProduct(new Product("A124", "Product 2", 100, 10.0));
     cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0));
@@ -39,5 +39,32 @@ describe("Catalogue", () => {
       expect(result).to.be.undefined;
     });
   });
+
+  describe("checkReorder", () => {
+   it("should return an empty array when no products need reordering", function () {
+     const result = cat.checkReorders();
+     expect(result.productIds).to.be.empty;
+   });
+   it("should report products that satisfy the reorder criteria", function () {
+     cat.addProduct(new Product("B123", "Product 4", 10, 20, 10.0));
+     cat.addProduct(new Product("B124", "Product 5", 10, 30, 10.0));
+     const result = cat.checkReorders();
+     expect(result.productIds).to.have.lengthOf(2);
+     expect(result.productIds).to.have.members(["B123", "B124"]);
+   });
+   it("should include products just on their reorder level", function () {
+     cat = new Catalogue("Test Catalogue");
+
+     cat.addProduct(new Product("B125", "Product 6", 10, 10, 10.0));
+     const result = cat.checkReorders();
+     expect(result.productIds).to.have.members(["B125"]);
+   });
+   it("should handle the empty catalogue case", function () {
+     cat = new Catalogue("Test Catalogue");
+
+     const result = cat.checkReorders();
+     expect(result.productIds).to.be.empty;
+    });
+ });
 
 });
